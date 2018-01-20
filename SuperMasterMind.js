ee340638eabd42e747a4d55d12b1eda622f0516d
supermastermind <31574054+supermastermind@users.noreply.github.com>
2018-01-20 11:30:18 +0100

    Add files via upload

--- SuperMasterMind.js
+++ SuperMasterMind.js
@@ -1585,7 +1585,7 @@ function draw_graphic_bis() {
 
       }
 
-    } while (resize_detected && (resize_cnt <= 50)); // several iterative calls are necessary to redraw the canvas with proper width and height on window resize
+    } while (resize_detected && (resize_cnt <= 75)); // several iterative calls are necessary to redraw the canvas with proper width and height on window resize
 
     let nbColumnsSelected = getNbColumnsSelected();
     if ( (nbColumnsSelected < 0) || (nbColumnsSelected > nbMaxColumns) ) { // (error case)
