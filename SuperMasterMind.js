752f684963acb42baaf221120d4b318de12a1132
supermastermind <31574054+supermastermind@users.noreply.github.com>
2018-01-20 12:08:57 +0100

    Add files via upload

--- SuperMasterMind.js
+++ SuperMasterMind.js
@@ -2515,7 +2515,7 @@ function draw_graphic_bis() {
         document.getElementById("showPossibleCodesButton").className = "button disabled";
       }
       else {
-        document.getElementById("showPossibleCodesButton").className = (androidMode ? "button" : "button");
+        document.getElementById("showPossibleCodesButton").className = (androidMode ? "button" /* ("button fast_blinking" generates display issues) */ : "button blinking");
       }
 
       if (CompressedDisplayMode) {
