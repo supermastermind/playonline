29f9c6cabb2a290ee9e6893720526efeea931425
supermastermind <31574054+supermastermind@users.noreply.github.com>
2018-06-11 17:36:49 +0200

    Add files via upload

--- SuperMasterMind.js
+++ SuperMasterMind.js
@@ -2352,27 +2352,27 @@ function draw_graphic_bis() {
             let multiply_factor;
             switch (nbColumns) {
               case 3:
-                nb_attempts_for_max_score = 2;
+                nb_attempts_for_max_score = 3; // (estimate: ~16% of scores, below 3 would not be relevant)
                 time_in_seconds_corresponding_to_one_attempt_in_score = 60.0; // (time corresponding to 2 attempts: 2 min)
                 multiply_factor = 0.50;
                 break;
               case 4:
-                nb_attempts_for_max_score = 3;
+                nb_attempts_for_max_score = 3; // (estimate: ~5% of scores)
                 time_in_seconds_corresponding_to_one_attempt_in_score = 300.0; // (time corresponding to 2 attempts: 10 min)
                 multiply_factor = 0.75;
                 break;
               case 5:
-                nb_attempts_for_max_score = 4;
+                nb_attempts_for_max_score = 4; // (estimate: ~5% of scores)
                 time_in_seconds_corresponding_to_one_attempt_in_score = 450.0; // (time corresponding to 2 attempts: 15 min) // See (*)
                 multiply_factor = 1.0;
                 break;
               case 6:
-                nb_attempts_for_max_score = 5;
+                nb_attempts_for_max_score = 5; // (estimate: ~7% of scores)
                 time_in_seconds_corresponding_to_one_attempt_in_score = 750.0;  // (time corresponding to 2 attempts: 25 min) // See (*)
                 multiply_factor = 1.5;
                 break;
               case 7:
-                nb_attempts_for_max_score = 6;
+                nb_attempts_for_max_score = 7; // (estimate: ~7% of scores)
                 time_in_seconds_corresponding_to_one_attempt_in_score = 1000.0;  // (time corresponding to 2 attempts: 33.3 min) // See (*)
                 multiply_factor = 2.0;
                 break;
