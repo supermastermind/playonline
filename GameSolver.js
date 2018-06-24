a9f3e72ac22227be8bed5e674b6ace4ed4298e9c
supermastermind <31574054+supermastermind@users.noreply.github.com>
2018-06-25 01:35:35 +0200

    Add files via upload

--- GameSolver.js
+++ GameSolver.js
@@ -55,7 +55,7 @@ let nbMaxAttemptsForEndOfGame = -1;
 let message_processing_ongoing = false;
 
 let init_refresh_time = 1222;
-let attempt_refresh_time_1 = 0;
+let attempt_refresh_time_1 = 444;
 let attempt_refresh_time_2 = 0;
 
 let max_performance_evaluation_time = 10001; // XXX 10001
