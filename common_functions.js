7de287061e76b85c39504115b209d6350ec45fbc
supermastermind <31574054+supermastermind@users.noreply.github.com>
2018-08-27 21:10:53 +0200

    Add files via upload

--- common_functions.js
+++ common_functions.js
@@ -16,28 +16,28 @@ function write_menu(selected_idx) {
               <li><a href='index.html'>Home</a></li>\
               <li class='selected'><a href='screenshots.html'><b>Screenshots</b></a></li>\
               <li><a href='optimal_strategy.html'>Optimal strategy</a></li>\
-              <li><a href='contact_info.html'>Contact info</a></li>";    
+              <li><a href='contact_info.html'>Contact info</a></li>";
       break;
     case 2:
       str = str + "\
               <li><a href='index.html'>Home</a></li>\
               <li><a href='screenshots.html'>Screenshots</a></li>\
               <li class='selected'><a href='optimal_strategy.html'><b>Optimal strategy</b></a></li>\
-              <li><a href='contact_info.html'>Contact info</a></li>";    
+              <li><a href='contact_info.html'>Contact info</a></li>";
       break;
     case 3:
       str = str + "\
               <li><a href='index.html'>Home</a></li>\
               <li><a href='screenshots.html'>Screenshots</a></li>\
               <li><a href='optimal_strategy.html'>Optimal strategy</a></li>\
-              <li class='selected'><a href='contact_info.html'><b>Contact info</b></a></li>";    
-      break;      
+              <li class='selected'><a href='contact_info.html'><b>Contact info</b></a></li>";
+      break;
     default:
       str = str + "\
               <li class='selected'><a href='index.html'><b>Home</b></a></li>\
               <li><a href='screenshots.html'>Screenshots</a></li>\
               <li><a href='optimal_strategy.html'>Optimal strategy</a></li>\
-              <li><a href='contact_info.html'>Contact info</a></li>";   
+              <li><a href='contact_info.html'>Contact info</a></li>";
   }
   str = str + "\
         </ul>\
@@ -62,7 +62,7 @@ function write_sidebar() {
   month[11] = "December";
   var d = new Date();
   var month_and_year_str = '(' + month[d.getMonth()] + ' ' + d.getFullYear() + ')';
-  
+
   document.write("\
     <div class='sidebar'>\
       <h1>History</h1>\
@@ -103,13 +103,14 @@ function write_introduction() {
     </a>");
 }
 
-function write_optimal_strategy_details(detail_level) {
-  var str = "<div id='strategy_details' onclick=\"document.getElementById('strategy_details').innerHTML='The optimal strategy is determined thanks to a <b>recursive algorithm</b> which goes through all possible games (enumeration of all games with some &quot;equivalent games&quot; tricks for optimization). The goal of this algorithm is to minimize the average number of attempts to find secret codes, all secret codes having the same weight (i.e. same probability to be selected).<br>In this algorithm, only the possible codes are considered at each stage of the game. In some (rare) cases, playing an impossible code may be better than playing the best possible code(s)! This could be called a &quot;useful mistake&quot;. ";
-  if (detail_level == 0) {
-    str += "This will thus result in the above number being strictly positive (e.g. <b>+0.10</b>). If you get such a positive number (voluntarily), you are really good!<br><div style=margin-top:5px;><a href=optimal_strategy.html><b>Optimal strategy statistics</b></a></div>';\"><font color=#A4AA04 style=cursor:pointer;><u><b>More details on the optimal strategy...</b></u></font></div><div style='margin-top:5px;'><a href=screenshots.html><b>Game examples & screenshots</b></a></div><br>";
-  }
-  else {
-    str += "</div><br>";
-  }
+var optimal_strategy_descr = "The optimal strategy is determined thanks to a <b>recursive algorithm</b> which goes through all possible games (enumeration of all games with some &quot;equivalent games&quot; tricks for optimization). The goal of this algorithm is to minimize the average number of attempts to find secret codes, all secret codes having the same weight (i.e. same probability to be selected).<br>In this algorithm, only the possible codes are considered at each stage of the game. ";
+
+function write_optimal_strategy_details_1() {
+  var str = "<div id='strategy_details' onclick=\"document.getElementById('strategy_details').innerHTML='" + optimal_strategy_descr + "In some (rare) cases, playing an impossible code may be better than playing the best possible code(s)! This could be called a &quot;useful mistake&quot;. ";
+  str += "This will thus result in the above number being strictly positive (e.g. <b>+0.10</b>). If you get such a positive number (voluntarily), you are really good!<br><div style=margin-top:5px;><a href=optimal_strategy.html><b>Optimal strategy statistics</b></a></div>';\"><font color=#A4AA04 style=cursor:pointer;><u><b>More details on the optimal strategy...</b></u></font></div><div style='margin-top:5px;'><a href=screenshots.html><b>Game examples & screenshots</b></a></div><br>";
   document.write(str);
+}
+
+function write_optimal_strategy_details_2() {
+  document.write(optimal_strategy_descr + "This strategy could thus be called the <b>optimal logical strategy</b>.<br>Other strategies exist which also allow playing impossible codes at each stage of the game (in addition to the possible codes). They offer slightly better statistics than the optimal logical strategy and are not considered here (very long processing times).");
 }
