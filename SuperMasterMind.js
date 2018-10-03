af3ee93ae26671eece8cce2b93f1b153a8ad5ec2
supermastermind <31574054+supermastermind@users.noreply.github.com>
2018-10-03 17:17:39 +0200

    Add files via upload

--- SuperMasterMind.js
+++ SuperMasterMind.js
@@ -16,7 +16,7 @@ console.log("Running SuperMasterMind.js...");
 // Main game variables
 // *******************
 
-let version = "v1.6";
+let version = "v1.65";
 
 let emptyColor = 0; // (0 is also the Java default table init value)
 let nbMinColors = 5;
