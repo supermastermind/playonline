ea9d3502b7597e3e2127315d2eaffca77ece1e49
supermastermind <31574054+supermastermind@users.noreply.github.com>
2018-10-07 10:02:30 +0200

    Add files via upload

--- SuperMasterMind.js
+++ SuperMasterMind.js
@@ -913,7 +913,7 @@ function displayRandomCodesHintIfNeeded() {
   if (randomCodesHintToBeDisplayed) {
     if (typeof(Storage) !== 'undefined') {
       if (localStorage.gamesok) {
-        if ( (Number(localStorage.gamesok) >= 100) && ((Number(localStorage.gamesok) % 25) == 0) ) {
+        if ( (Number(localStorage.gamesok) >= 100) && ((Number(localStorage.gamesok) % 50) == 0) ) {
           alert("A little fun?!\nClick on the \"" + document.getElementById("playRandomCodeButton").value + "\" button to play your first codes randomly!");
           randomCodesHintToBeDisplayed = false;
         }
