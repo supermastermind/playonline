ca2b652cd2c7ca31ecd6d913a2c0fb8e06a3756d
supermastermind <31574054+supermastermind@users.noreply.github.com>
2018-12-06 22:38:31 +0100

    Add files via upload

--- GameSolver.js
+++ GameSolver.js
@@ -2992,7 +2992,7 @@ try {
             break;
           case 5:
             nbMaxMarks = 20;
-            maxPerformanceEvaluationTime = baseOfMaxPerformanceEvaluationTime*50/30;
+            maxPerformanceEvaluationTime = baseOfMaxPerformanceEvaluationTime*60/30;
             nbOfCodesForSystematicEvaluation = Math.min(baseOfNbOfCodesForSystematicEvaluation, initialNbPossibleCodes);
             nbOfCodesForSystematicEvaluation_ForMemAlloc = initialNbPossibleCodes; // game precalculation (*)
             initialNbClasses = 7; // {11111, 11112, 11122, 11123, 11223, 11234, 12345}
@@ -3002,7 +3002,7 @@ try {
             break;
           case 6:
             nbMaxMarks = 27;
-            maxPerformanceEvaluationTime = baseOfMaxPerformanceEvaluationTime*65/30;
+            maxPerformanceEvaluationTime = baseOfMaxPerformanceEvaluationTime*70/30;
             nbOfCodesForSystematicEvaluation = Math.min(baseOfNbOfCodesForSystematicEvaluation, initialNbPossibleCodes);
             nbOfCodesForSystematicEvaluation_ForMemAlloc = nbOfCodesForSystematicEvaluation;
             initialNbClasses = 11; // {111111, 111112, 111122, 111123, 111222, 111223, 111234, 112233, 112234, 112345, 123456}
@@ -3023,7 +3023,7 @@ try {
             // *                *** TOTAL: 35 marks *** *
             // ******************************************
             nbMaxMarks = 35;
-            maxPerformanceEvaluationTime = baseOfMaxPerformanceEvaluationTime*75/30;
+            maxPerformanceEvaluationTime = baseOfMaxPerformanceEvaluationTime*80/30;
             nbOfCodesForSystematicEvaluation = Math.min(baseOfNbOfCodesForSystematicEvaluation, initialNbPossibleCodes);
             nbOfCodesForSystematicEvaluation_ForMemAlloc = nbOfCodesForSystematicEvaluation;
             initialNbClasses = 15; // {1111111, 1111112, 1111122, 1111123, 1111222, 1111223, 1111234, 1112223, 1112233, 1112234, 1112345, 1122334, 1122345, 1123456, 1234567}
