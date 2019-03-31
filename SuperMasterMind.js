809b073b228c2b576d40c02f83a967d34dcb4a43
supermastermind <31574054+supermastermind@users.noreply.github.com>
2019-03-31 19:16:04 +0200

    Add files via upload

--- SuperMasterMind.js
+++ SuperMasterMind.js
@@ -16,7 +16,7 @@ console.log("Running SuperMasterMind.js...");
 // Main game variables
 // *******************
 
-let version = "v2.72";
+let version = "v2.73";
 
 let emptyColor = 0; // (0 is also the Java default table init value)
 let nbMinColors = 5;
--- common_functions.js
+++ common_functions.js
@@ -62,7 +62,7 @@ function write_sidebar() {
   document.write("\
     <div class='sidebar'>\
       <h1>History</h1>\
-      <b>Current version: 2.72<br>"
+      <b>Current version: 2.73<br>"
       + month_and_year_str +
       "</b><br><br>\
       Version 2.0<br>\
