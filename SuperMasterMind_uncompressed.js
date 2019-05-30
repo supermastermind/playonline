51b58bb1fc451cc4bf3b58b898111f7438a32e50
supermastermind <31574054+supermastermind@users.noreply.github.com>
2019-05-30 09:11:22 +0200

    Add files via upload

--- SuperMasterMind_uncompressed.js
+++ SuperMasterMind_uncompressed.js
@@ -1093,7 +1093,7 @@ function displayRandomCodesHintIfNeeded() {
     if (typeof(Storage) !== 'undefined') {
       if (localStorage.gamesok) {
         if ( (Number(localStorage.gamesok) >= 100) && ((Number(localStorage.gamesok) % 50) == 0) ) {
-          alert("A little fun?!\nClick on the \"" + document.getElementById("playRandomCodeButton").value + "\" button to play your first codes randomly!");
+          alert("A little fun?!\nClick on the \"" + document.getElementById("playRandomCodeButton").value + "\" button to play your first few codes randomly!");
           randomCodesHintToBeDisplayed = false;
         }
       }
