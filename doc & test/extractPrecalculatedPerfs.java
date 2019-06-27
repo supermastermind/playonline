25e54beeee538b38f52015db808eba8fa6289fa7
supermastermind <31574054+supermastermind@users.noreply.github.com>
2019-06-27 09:50:06 +0200

    Add files via upload

--- doc & test/extractPrecalculatedPerfs.java
+++ doc & test/extractPrecalculatedPerfs.java
@@ -28,100 +28,142 @@ public class extractPrecalculatedPerfs {
       "5 of 5 Precalculated perfs in range 270..1500 and 2.7 seconds for 12345 first code.txt"
     };
 
-  private static int table_tmp[] = new int[9];
-  private static int previous_table_tmp[] = new int[9];
-  private static String output_suffix = null;
-  private static String determine_5_columns_code_type(String code_str, String previous_code_str) throws Exception {
+  private static int table_tmp[][] = new int[2][9];
+  private static boolean are_there_5_identical_colors[] = new boolean[2];
+  private static boolean are_there_4_identical_colors[] = new boolean[2];
+  private static boolean is_there_triple[] = new boolean[2];
+  private static int nb_doubles[] = new int[2];
+  private static int one_double_color[] = new int[2];
+  private static String output_str[] = new String[2];
+  private static String determine_smm_output_filename(String code_str_1, String mark_str_1, String code_str_2, String mark_str_2) throws Exception {
 
-  output_suffix = "";
-  int totalnbcolors = 0;
+  // Handle each couple (code, mark)
+  // *******************************
 
-  if (code_str.length() != 5) {
-    throw new Error("unexpected code_str size: " + code_str.length());
-  }
-  for (int color = 0; color < table_tmp.length; color++) {
-    table_tmp[color] = 0;
-  }
-  for (int column = 0; column < 5; column++) {
-    int color = code_str.charAt(column) - 48;
-    table_tmp[color]++;
-  }
-  boolean are_there_5_identical_colors = false;
-  boolean are_there_4_identical_colors = false;
-  boolean is_there_triple = false;
-  int nb_doubles = 0;
-  int one_double_color = 0;
-  for (int color = 0; color < table_tmp.length; color++) {
-    if (table_tmp[color] == 5) {
-      are_there_5_identical_colors = true;
-      break;
-    }
-    else if (table_tmp[color] == 4) {
-      are_there_4_identical_colors = true;
-      break;
-    }
-    else if (table_tmp[color] == 3) {
-      is_there_triple = true;
-    }
-    else if (table_tmp[color] == 2) {
-      nb_doubles++;
-      one_double_color = color;
-    }
-  }
+  for (int code_idx = 0; code_idx < 2; code_idx++) {
 
-  if (previous_code_str != null) {
-    if (previous_code_str.length() != 5) {
-      throw new Error("unexpected previous_code_str size: " + previous_code_str.length());
+    String code_str;
+    String mark_str;
+    switch (code_idx) {
+      case 0:
+        code_str = code_str_1;
+        mark_str = mark_str_1;
+        break;
+      case 1:
+        code_str = code_str_2;
+        mark_str = mark_str_2;
+        break;
+      default:
+        throw new Error("internal error in determine_smm_output_filename (code_idx)");
     }
-    for (int color = 0; color < previous_table_tmp.length; color++) {
-      previous_table_tmp[color] = 0;
+
+    if (code_str.length() != 5) {
+      throw new Error("invalid code_str: " + code_str);
+    }
+    if ((mark_str.length() != 4) || (mark_str.indexOf("B") == -1) || (mark_str.indexOf("W") == -1)) {
+      throw new Error("invalid mark_str: " + mark_str);
+    }
+
+    for (int color = 0; color < table_tmp[code_idx].length; color++) {
+      table_tmp[code_idx][color] = 0;
     }
     for (int column = 0; column < 5; column++) {
-      int color = previous_code_str.charAt(column) - 48;
-      previous_table_tmp[color]++;
+      int color = code_str.charAt(column) - 48;
+      table_tmp[code_idx][color]++;
     }
-    for (int color = 0; color < previous_table_tmp.length; color++) {
-      if ((table_tmp[color] > 0) || (previous_table_tmp[color] > 0)) {
-        totalnbcolors++;
+
+    are_there_5_identical_colors[code_idx] = false;
+    are_there_4_identical_colors[code_idx] = false;
+    is_there_triple[code_idx] = false;
+    nb_doubles[code_idx] = 0;
+    one_double_color[code_idx] = 0;
+
+    for (int color = 0; color < table_tmp[code_idx].length; color++) {
+      if (table_tmp[code_idx][color] == 5) {
+        are_there_5_identical_colors[code_idx] = true;
+        break;
+      }
+      else if (table_tmp[code_idx][color] == 4) {
+        are_there_4_identical_colors[code_idx] = true;
+        break;
+      }
+      else if (table_tmp[code_idx][color] == 3) {
+        is_there_triple[code_idx] = true;
+      }
+      else if (table_tmp[code_idx][color] == 2) {
+        nb_doubles[code_idx]++;
+        one_double_color[code_idx] = color;
       }
     }
-    if (nb_doubles == 1) { // many combinations in case of 1 double => a suffix is necessary / 1 double => this color is not equivalent to any other
-       output_suffix = "_" + Integer.toString(previous_table_tmp[one_double_color]) + "X";
+
+    if (are_there_5_identical_colors[code_idx]) {
+      output_str[code_idx] = "5";
     }
-    output_suffix = output_suffix + "_" + Integer.toString(totalnbcolors) + "Y";
+    else if (are_there_4_identical_colors[code_idx]) {
+      output_str[code_idx] = "4+1";
+    }
+    else if (is_there_triple[code_idx]) {
+      if (nb_doubles[code_idx] == 0) {
+        output_str[code_idx] = "3+1+1";
+      }
+      else if (nb_doubles[code_idx] == 1) {
+        output_str[code_idx] = "3+2";
+      }
+      else {
+        throw new Error("internal error: triple with several doubles");
+      }
+    }
+    else {
+      if (nb_doubles[code_idx] == 0) {
+        output_str[code_idx] = "1+1+1+1+1";
+      }
+      else if (nb_doubles[code_idx] == 1) {
+        output_str[code_idx] = "2+1+1+1";
+      }
+      else if (nb_doubles[code_idx] == 2) {
+        output_str[code_idx] = "2+2+1";
+      }
+      else {
+        throw new Error("internal error: invalid number of doubles");
+      }
+    }
+    output_str[code_idx] = output_str[code_idx] + "_" + mark_str;
+
+  } // end for code_idx
+
+  // Determine output filename
+  // *************************
+
+  int totalnbcolors = 0;
+  for (int color = 0; color < table_tmp[0].length; color++) {
+    if ((table_tmp[0][color] > 0) || (table_tmp[1][color] > 0)) {
+      totalnbcolors++;
+    }
   }
 
-  if (are_there_5_identical_colors) {
-    return "5C";
-  }
-  else if (are_there_4_identical_colors) {
-    return "4+1C";
-  }
-  else if (is_there_triple) {
-    if (nb_doubles == 0) {
-      return "3+1+1C";
-    }
-    else if (nb_doubles == 1) {
-      return "3+2C";
-    }
-    else {
-      throw new Error("internal error: triple with several doubles");
-    }
-  }
-  else {
-    if (nb_doubles == 0) {
-      return "1+1+1+1+1C";
-    }
-    else if (nb_doubles == 1) {
-      return "2+1+1+1C";
-    }
-    else if (nb_doubles == 2) {
-      return "2+2+1C";
-    }
-    else {
-      throw new Error("internal error: too many doubles");
+  String suffix = "";
+  suffix = "_" + Integer.toString(totalnbcolors) + "X";
+  if ((!is_there_triple[1]) && (nb_doubles[1] == 1)) { // many combinations in case of 1 double => a suffix is necessary / 1 double => this color is not equivalent to any other
+    suffix = suffix + "_" +  Integer.toString(table_tmp[0][one_double_color[1]]) + "Y";
+    if ((!is_there_triple[0]) && (nb_doubles[0] == 1)) { // many combinations in case of 1 double => an extra suffix is necessary
+      int nb_double_intersections = 0;
+      for (int column = 0; column < 5; column++) {
+        int color_1 = code_str_1.charAt(column) - 48;
+        if (color_1 == one_double_color[0]) {
+          int color_2 = code_str_2.charAt(column) - 48;
+          if (color_2 == one_double_color[1]) {
+            nb_double_intersections++;
+          }
+        }
+      }
+      suffix = suffix + "_" + Integer.toString(nb_double_intersections) + "Z";
     }
   }
+
+  // Return filename
+  // ***************
+
+  return output_str[0] + "_" + output_str[1] + suffix + ".js";
 
  }
 
@@ -148,7 +190,7 @@ public class extractPrecalculatedPerfs {
             }
             cnt++;
 
-            String output_filename = "out/" + determine_5_columns_code_type(matcher.group(1), null) + "_" + matcher.group(2) + "_" + determine_5_columns_code_type(matcher.group(3), matcher.group(1)) + "_" + matcher.group(4) + output_suffix + ".js";
+            String output_filename = "out/" + determine_smm_output_filename(matcher.group(1), matcher.group(2), matcher.group(3), matcher.group(4));
             File output_file = new File(output_filename);
             boolean write_prefix = false;
             if(!output_file.exists()){
@@ -158,9 +200,11 @@ public class extractPrecalculatedPerfs {
             FileWriter fw = new FileWriter(output_file, true /* (append) */);
             BufferedWriter bw = new BufferedWriter(fw);
             if (write_prefix) {
-              bw.write("let extra_precalculated_str = \"\"\n");
+              bw.write("let extra_precalculated_str = " + line.replace(" +","") + "\n");
             }
-            bw.write("+" + line.replace(" +","") + "\n");
+            else {
+             bw.write("+" + line.replace(" +","") + "\n");
+            }
             bw.close();
           }
 
