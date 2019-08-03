3213d2ab2223171081f7c5e824dd71f4210f9dba
supermastermind <31574054+supermastermind@users.noreply.github.com>
2019-08-03 21:00:02 +0200

    Add files via upload

--- SuperMasterMind_uncompressed.js
+++ SuperMasterMind_uncompressed.js
@@ -1457,13 +1457,13 @@ function getNbColumnsSelected() {
 }
 
 function show_play_store_app(forcedMode = false) {
-  if ( (androidMode && (!android_appli))
+  if( (androidMode && (!android_appli))
        || forcedMode ) {
     let play_store_app_str =
       "<center><table style='width:" + rulesTableWidthStr + ";'><tr style='text-align:center;'><td><font style='font-size:1.75vh;color:black'>\
       <br><b>For a better game experience, install the android app!</b><br>\
       <a href='https://play.google.com/store/apps/details?id=supermastermind.github.io&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' style='height:12vh;margin-top:1.5vh;margin-bottom:1.5vh' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/><img alt='Get it on Google Play' style='height:12vh;margin-top:1.5vh;margin-bottom:1.5vh' src='img/Playstore_icon.png'/><br></a>\
-      For any suggestion for improvement, please use the contact info page... and if you like this app, put some stars in Google Play Store!&nbsp;&#x1F609;<br><br><br>\
+      <small>If you like this app, put some stars in Google Play Store! &#x1F609; For any suggestion for improvement, please see the <a href='contact_info.html'>contact info</a> page.</small><br><br><br>\
       </font></td></tr></table></center>";
     try {
       modal_mode = 4;
@@ -1633,7 +1633,7 @@ function resetGameAttributes(nbColumnsSelected) {
       location.reload(true);
     }
   }
-  if (game_cnt == 4) {
+  if ((game_cnt == 4) || (localStorage.gamesok && (Number(localStorage.gamesok) % 100 == 0))) {
     show_play_store_app();
   }
 
