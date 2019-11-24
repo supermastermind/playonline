8de0dc6b570e10cb48bf295aed8636b3e503a4a5
supermastermind <31574054+supermastermind@users.noreply.github.com>
2019-11-24 21:57:27 +0100

    Add files via upload

--- common_functions.js
+++ common_functions.js
@@ -95,10 +95,10 @@ function write_introduction() {
     <a href='game.html' onfocus='this.blur()'>\
       <div id='game_images'>");
   var img_str = "<hr style='height:0.75vh; visibility:hidden;' /><img src='img/SuperMasterMind_1.png' style='width:4.5rem;border:0;margin:0px'><img src='img/SuperMasterMind_2.png' style='width:4.5rem;border:0;margin:0px'><img src='img/SuperMasterMind_3.png' style='width:4.5rem;border:0;margin:0px'>";
-  if (window.innerWidth >= 555) {
+  if (window.innerWidth >= 400) {
     img_str = img_str + "<img src='img/SuperMasterMind_4.png' style='width:4.5rem;border:0;margin:0px'>";
   }
-  if (window.innerWidth >= 777) {
+  if (window.innerWidth >= 500) {
     img_str = img_str + "<img src='img/SuperMasterMind_5.png' style='width:4.5rem;border:0;margin:0px'>";
   }
   document.write(img_str);
