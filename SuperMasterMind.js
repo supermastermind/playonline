7bf8185eaa69388274ba0d1d909ebe610e03ed60
supermastermind <31574054+supermastermind@users.noreply.github.com>
2019-12-05 00:35:11 +0100

    Add files via upload

--- SuperMasterMind.js
+++ SuperMasterMind.js
@@ -186,7 +186,7 @@ let foregroundColorTable=
 if(localStorage.modernDisplay){
 modernDisplay=(localStorage.modernDisplay=="true");
 }
-let legacy_backgroundColor_2_base_color="#894B0F";// "#7F4613";// "#694927";
+let legacy_backgroundColor_2_base_color="#7F4613";// "#894B0F";// "#694927";
 let backgroundColor_2;
 let backgroundColor_3;
 let highlightColor;
