eee147b68575569d904a55269d7aced88db03709
supermastermind <31574054+supermastermind@users.noreply.github.com>
2020-02-02 13:34:36 +0100

    Add files via upload

--- GameSolver_uncompressed.js
+++ GameSolver_uncompressed.js
@@ -2507,7 +2507,7 @@ try {
           }
         }
         // Precalculation optimization (2/4): skip impossible cur code if acceptable
-        if ((next_cur_game_idx >= 2) && (nbCodes <= nbCodesForPrecalculationThreshold)) { // (***)
+        if ( (next_cur_game_idx >= 2) && (nbCodes <= nbCodesForPrecalculationThreshold) ) { // (***) - not adapted to simple codes for which nbCodesForPrecalculationThreshold shall keep realisticly low
           skip_cur_code = true;
         }
         if (skip_cur_code) {
@@ -3061,7 +3061,7 @@ try {
       str = "\"" + str.substring(0, str.length-1) + ".\" +"; // remove last ','
       // console.log(str);
       let precalculation_time = new Date().getTime() - precalculation_start_time;
-      if (precalculation_time >= 2700) { // 2700 = 2.7 seconds on i5 processor or on Linux VB running on i7 processor
+      if (precalculation_time >= 2500) { // 2500 = 2.5 seconds on i5 processor or on Linux VB running on i7 processor
         send_trace_msg(str + " // " + precalculation_time + "ms");
       }
       else {
