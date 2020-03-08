3df52634202fc3b986d0d2e26913be0920df3e03
supermastermind <31574054+supermastermind@users.noreply.github.com>
2020-03-08 19:48:14 +0100

    Add files via upload

--- SuperMasterMind.js
+++ SuperMasterMind.js
@@ -1562,14 +1562,14 @@ score=-1.0;
 sumPerfs=0.00;
 nbUnknownPerfs=0;
 sCode=~(simpleCodeHandler.createRandomCode());
-/* XXX */
+/* XXX
 let toto=simpleCodeHandler.createRandomCode();
 toto=simpleCodeHandler.setColor(toto, 4, 1);
-toto=simpleCodeHandler.setColor(toto, 3, 2);
-toto=simpleCodeHandler.setColor(toto, 7, 3);
-toto=simpleCodeHandler.setColor(toto, 6, 4);
-toto=simpleCodeHandler.setColor(toto, 1, 5);
-sCode=~(toto);
+toto=simpleCodeHandler.setColor(toto, 4, 2);
+toto=simpleCodeHandler.setColor(toto, 4, 3);
+toto=simpleCodeHandler.setColor(toto, 4, 4);
+toto=simpleCodeHandler.setColor(toto, 4, 5);
+sCode=~(toto); */
 sCodeRevealed=0;
 newGameEvent=false;
 playerWasHelped=false;
