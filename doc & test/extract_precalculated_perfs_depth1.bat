39f79a2469077f05e96dcdadda13d5fd85bd5938
supermastermind <31574054+supermastermind@users.noreply.github.com>
2020-04-15 19:04:41 +0200

    Add files via upload

--- doc & test/extract_precalculated_perfs_depth1.bat
+++ doc & test/extract_precalculated_perfs_depth1.bat
@@ -1,6 +1,6 @@
 @echo OFF
 
-set precalc_files="1 of 5 Precalculated perfs in range 300..1300 and 3.5 seconds for 11111, 11112 and 11122 first codes.txt" "2 of 5 Precalculated perfs in range 270..1500 and 2.7 seconds for 11123 first code.txt" "3 of 5 Precalculated perfs in range 270..1500 and 2.7 seconds for 11223 first code.txt" "4 of 5 Precalculated perfs in range 270..1500 and 2.7 seconds for 11234 first code.txt" "5 of 5 Precalculated perfs in range 270..1500 and 2.7 seconds for 12345 first code.txt"
+set precalc_files="STAGE2 - RESULTS_300_to_1300_3.5sec_11111 (OK).txt" "STAGE1 - 1 of 5 Precalculated perfs in range 300..1300 and 3.5 seconds for 11112 and 11122 first codes.txt"       "STAGE2 - RESULTS_270_to_1700_2.7sec_11123 (OK).txt"       "STAGE2 - RESULTS_270_to_1700_2.7sec_11223 (OK).txt"       "STAGE2 - RESULTS_270_to_1700_2.7sec_11234 (OK).txt"       "STAGE2 - RESULTS_270_to_1700_2.7sec_12345 (OK).txt"
 
 echo "******************** 4B0W ********************"
 grep -h "4B0W" %precalc_files% | grep -v "comp" | grep '^"1'
---
+++ doc & test/extract_precalculated_perfs_specific.bat
@@ -0,0 +1,3 @@
+@echo OFF
+
+findstr "\"2" "2 of 5 Precalculated perfs in range 270..1500 and 2.7 seconds for 11123 first code.txt"
