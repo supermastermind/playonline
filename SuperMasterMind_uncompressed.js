a9c7ab4c5bd05413839c41caec84a34d484bdb8d
supermastermind <31574054+supermastermind@users.noreply.github.com>
2020-05-20 23:05:37 +0200

    Add files via upload

--- SuperMasterMind_uncompressed.js
+++ SuperMasterMind_uncompressed.js
@@ -1955,12 +1955,17 @@ function resetGameAttributes(nbColumnsSelected) {
   if ((next_code1 != 0) && (next_code2 != 0) && (next_code3 == 0) && (next_scode != 0)) {
     worst_mark_alert_already_displayed = true; // (avoid multiple alerts)
     sCode = next_scode;
+    gameInv = 1;
     setTimeout("playACodeAutomatically(" + next_code1 + ");playACodeAutomatically(" + next_code2 + ");updateAndStoreNbGamesStarted(-1);", 44);
   }
   else if ((next_code1 != 0) && (next_code2 != 0) && (next_code3 != 0) && (next_scode != 0)) {
     worst_mark_alert_already_displayed = true; // (avoid multiple alerts)
     sCode = next_scode;
+    gameInv = 2;
     setTimeout("playACodeAutomatically(" + next_code1 + ");playACodeAutomatically(" + next_code2 + ");playACodeAutomatically(" + next_code3 + ");updateAndStoreNbGamesStarted(-1);", 44);
+  }
+  else {
+    gameInv = 0;
   }
   next_code1 = 0; // (empty code)
   next_code2 = 0; // (empty code)
