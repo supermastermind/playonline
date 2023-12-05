5da0c6585958768da30223fc3ed5784b30fd434f
supermastermind <31574054+supermastermind@users.noreply.github.com>
2023-12-05 23:18:18 +0100

    Add files via upload

--- SuperMasterMind.js
+++ SuperMasterMind.js
@@ -1213,7 +1213,7 @@ let play_store_app_str=
 "<center><table style='width:"+rulesTableWidthStr+";'><tr style='text-align:center;'><td><font style='font-size:1.75vh;color:black'>\
 <br><b>"+str+"</b><br>\
 <a href='"+android_app_url+"'><img alt='Get it on Google Play' style='height:11vh;margin-top:1.5vh;margin-bottom:1.5vh' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/><img alt='Get it on Google Play' style='height:11vh;margin-top:1.5vh;margin-bottom:1.5vh;border-radius:7%' src='img/Playstore_icon.png'/><br></a>\
-<small>"+(((specific_str.indexOf("put some stars")==-1)&&(str.indexOf("install the android app")==-1)) ? "If you like this game,&nbsp;put some stars on Google Play &#x1F609;<br>" : "")+"Any questions or comments? See&nbsp;the&nbsp;<a href='contact_info.html'>Contact</a>&nbsp;page</small><br><br><br>\
+<small>"+(((specific_str.indexOf("put some stars")==-1)&&(str.indexOf("install the android app")==-1)) ? "If you like this game,&nbsp;put some stars on Google Play &#x1F609;<br>" : "")+"</small><br>\
 </font></td></tr></table></center>";
 try{
 modal_mode=4;
