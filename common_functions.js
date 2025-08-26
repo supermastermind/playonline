2beb2f3c4211751e10fd3f609fdf99ab90fb21ca
supermastermind <31574054+supermastermind@users.noreply.github.com>
2025-08-27 01:24:57 +0200

    Add files via upload

--- common_functions.js
+++ common_functions.js
@@ -59,17 +59,16 @@ function set_div_contents_several_times(cmd_str, targeted_nb_ticks, periodical_t
   }
 }
 
-var optimal_strategy_descr = "The optimal strategy is determined thanks to a recursive algorithm which goes through all possible games (enumeration of all games with some &quot;equivalent games&quot; rules for optimization). The goal of this algorithm is to minimize the average number of attempts to find secret codes, all secret codes having the same probability to be selected.<br>In this algorithm, only the logical (i.e. possible) codes are considered at each stage of the game. ";
+var optimal_strategy_descr = "The optimal strategy is determined using a recursive algorithm which explores all possible games, applying \"equivalent game\" rules to optimize the enumeration process. The goal of this algorithm is to minimize the average number of attempts to find secret codes, all secret codes having the same probability to be selected.<br>In this algorithm, only the possible codes are considered at each stage of the game. ";
 
 function write_optimal_strategy_details_1() {
-  var str = "<div>" + optimal_strategy_descr + "In some situations, playing an illogical code may be better than playing the best logical code(s). This could be called a &quot;useful mistake&quot;. "
+  var str = "<div>" + optimal_strategy_descr + "In some situations, playing an impossible code may be better than playing the best possible code(s). This could be called a &quot;useful mistake&quot;. "
             + "This will result in a strictly positive performance for this code (e.g. <b><font color=#008200>+0.25</font></b>). If you get such a positive performance (voluntarily), you are good!&nbsp;&#x1F914;"
             + "<hr style='height:0.5rem; visibility:hidden;' />"
-            + "<hr style='height:0.5rem; visibility:hidden;' />"
-            + "See <a href=optimal_strategy.html><b>Optimal strategy</b></a> for more details.</div>";
+            + "For more details: <a href=optimal_strategy.html><b>Optimal strategy</b></a></div>";
   document.write(str);
 }
 
 function write_optimal_strategy_details_2() {
-  document.write(optimal_strategy_descr + "This strategy could thus be called the <b><font color=#CC0099>optimal logical strategy</font></b>. It is implemented in the <a href=index.html>online game</a> and <a href='https://play.google.com/store/apps/details?id=supermastermind.github.io'>android app</a>.<hr style='height:0.75vh; visibility:hidden;' />Other strategies exist in which \"any code\" can be played at each stage of the game, that's to say both logical codes and illogical codes. Mathematically optimal strategies are among them, but processing times to fully evaluate them can be very long, especially for Super Master Mind and more complex games. They offer slightly better statistics than the <b><font color=#CC0099>optimal logical strategy</font></b>. Some comparison is made in this page.");
+  document.write(optimal_strategy_descr + "This strategy could thus be called the <b><font color=#CC0099>optimal logical strategy</font></b>. It is implemented in the <a href=index.html>online game</a> and <a href='https://play.google.com/store/apps/details?id=supermastermind.github.io'>Android app</a>.<hr style='height:0.75vh; visibility:hidden;' />Other strategies exist in which \"any code\" can be played at each stage of the game, that's to say both possible and impossible codes. Mathematically optimal strategies are among them, but processing times to fully evaluate them can be very long, especially for Super Master Mind and more complex games. They offer slightly better statistics than the <b><font color=#CC0099>optimal logical strategy</font></b>. Some comparison is made in this page.");
 }
